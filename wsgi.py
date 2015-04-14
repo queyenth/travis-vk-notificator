52b755a9c1f9a19bb7259c0cefee91e154a838e0
Queyenth <queyenth@gmail.com>
2015-04-14 14:51:46 +0600

    Implemented #1 and deleted rhcloud staff

--- wsgi.py
+++ wsgi.py
@@ -1,60 +1,100 @@
 #!/usr/bin/python
 import os
 import cgi
-import vk
+from cgi import parse_qs, escape
+from wsgiref.simple_server import make_server
+import urllib
 import json
+import vk
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+is_auth = False
+vkapi = vk.API
 
-EVGENY_ACCESS_TOKEN='#####################################################################################'
-USER_ID=283030161
+# $c - commit
+# $n - build num
+# $o - owner
+# $m - message
+# $s - status message
+# $r - repo name
+# $rid - repo id
+# $bid - build id
+# $rurl - repo url
+# $burl - build url
+# $started - started time
+# $finished - finished build time
+# $duration - duration
+# $branch - branch
+# $compareurl - compare commit url
+# $commitername - commiter name
+# $commitermail - commiter mail
+# $tag - tag
+# $pullrequest - pull request
+# $pullrequestnum - pull request number
+# $pullrequesttitle - pull request title
+# Standart format: $o/$r build number #$n: $s\ncommit: $m\n$burl
 
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
+def format_message(buffer, message_format):
+    decoded = json.loads(buffer)
+    repo = decoded['repository']
+    formats = { "$pullrequestnum": decoded['pull_request_number'],
+                "$pullrequesttitle": decoded['pull_request_title'],
+                "$pullrequest": decoded['pull_request'],
+                "$rid": str(repo['id']),
+                "$bid": str(decoded['id']),
+                "$rurl": 'https://github.com/'+repo['owner_name']+'/'+repo['name'],
+                "$burl": decoded['build_url'],
+                "$started": decoded['started_at'],
+                "$finished": decoded['finished_at'],
+                "$duration": str(decoded['duration']),
+                "$branch": decoded['branch'],
+                "$compareurl": decoded['compare_url'],
+                "$commitername": decoded['committer_name'],
+                "$commitermail": decoded['committer_email'],
+                "$tag": decoded['tag'],
+                "$c": decoded['commit'],
+                "$n": decoded['number'],
+                "$o": repo['owner_name'],
+                "$m": decoded['message'],
+                "$s": decoded['status_message'],
+                "$r": repo['name'],
+                "\\n": "\n"
+    }
+    message = message_format
+    for key, value in formats.iteritems():
+        if isinstance(value, basestring):
+            message = message.replace(key, value)
+    return message
 
 def application(environ, start_response):
+    global is_auth
+    global vkapi
 
-    html = ""
+    d = parse_qs(environ['QUERY_STRING'])
+    user_ids = d.get('ids', [])
+    user_ids = [escape(user_id) for user_id in user_ids]
+    
+    message_format = d.get('format', [])
+    if message_format:
+        message_format = urllib.unquote(escape(message_format[0])).decode('utf8')
+    else:
+        message_format = "$o/$r build number #$n: $s\ncommit: $m\n$burl"
+
     if environ['REQUEST_METHOD'] == 'POST':
-        post_environ = environ.copy()
-        post_environ['QUERY_STRING'] = ''
-        post = cgi.FieldStorage(
-            fp=environ['wsgi.input'],
-            environ=post_environ,
-            keep_blank_values=True
-        )
+        length = int(environ.get('CONTENT_LENGTH', '0'))
 
-        decoded = json.loads(post['payload'].value)
-        repository = decoded['repository']
-        repo = repository['name']
-        owner = repository['owner_name']
-        build_num = decoded['number']
-        build_url = decoded['build_url']
-        message = decoded['message']
-        result = decoded['status_message']
-
-        new_message = owner + "/" + repo + " build number #" + build_num + ": " + result + "\ncommit: " + message + "\n" + build_url
-        vkapi = vk.API(access_token=EVGENY_ACCESS_TOKEN)
-        vkapi.messages.send(user_ids=USER_ID, message=new_message)
-
+        if not is_auth:
+            vkapi = vk.API(app_id=app_id, user_login=user_login, user_password=user_password, scope="messages,friends")
+            is_auth = True
+        
+        message = format_message(environ['wsgi.input'].read(length), message_format) 
+        vkapi.messages.send(user_ids=user_ids, message=message)
+    
     status = '200 OK'
-    response_headers = [('Content-Type', "text/plain"), ('Content-Length', str(len(html)))]
-    #
+    response_headers = [('Content-Type', "text/plain"), ('Content-Length', str(len("")))]
     start_response(status, response_headers)
     return [""]
 
-#
-# Below for testing only
-#
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
+    print "Started server..."
     httpd = make_server('localhost', 8051, application)
-    # Wait for a single request, serve it and quit.
     httpd.serve_forever()
